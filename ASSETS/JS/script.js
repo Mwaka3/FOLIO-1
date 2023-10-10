df4c8cfed33d4a61d3d672557d3761b7e747e4f8
Maxwell <maxwellmwaka3@gmail.com>
2023-10-11 00:27:24 +0300

    Initial Commit

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,37 @@
+/* -----------HOME PAGE------------- */
+var typed = new Typed(".multiple-text", {
+    strings: ["Front End Developer", "Technical Support Engineer", "QA Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+/* ======================FOR THE EXPERIENCE TABS===================== */
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+/* ----------PROJECTS SWIPER----------- */
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
